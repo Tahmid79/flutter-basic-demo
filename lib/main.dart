8913ab4ceddc8e1f1c61da0336ca9823ceb785ca
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-03 01:15:45 +0600

    add list tile code

--- lib/main.dart
+++ lib/main.dart
@@ -23,3 +23,38 @@ class MyApp extends StatelessWidget {
         ));
   }
 }
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  
+  final _suggestions = <WordPair>[] ;
+  final _saved = <WordPair>{} ; // NEW
+  final _biggerFont = TextStyle(fontSize: 18) ;
+
+  Widget _buildRow(WordPair pair){
+    final alreadySaved = _saved.contains(pair) ; // NEW
+    return ListTile(
+      title: Text(
+        pair.asPascalCase ,
+        style: _biggerFont
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color : alreadySaved ? Colors.red : null ,
+        semanticLabel: alreadySaved ? 'Remove from save' : 'Save',
+      )
+    ) ;
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
