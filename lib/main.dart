6367d0ed20bc26a75336379f6cfbe52d9e24a7a6
Tahmid Farhan <tahmidfarhan12@gmail.com>
2022-03-04 23:37:25 +0600

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,135 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp()) ;
+  runApp(const MyApp());
 }
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // #docregion build
+  @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random() ;
-    return MaterialApp(
-        title: 'Welcome to Flutter',
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Welcome to flutter'),
-          ),
-          body:  Center(
-              // child: const Text('Hello World')
-            child: Text(wordPair.asPascalCase)
-          ),
-        ));
+    return const MaterialApp(
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
   }
+// #enddocregion build
 }
+// #enddocregion MyApp
+
+// #docregion RWS-var
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
+
+  // #docregion RWS-build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions : [
+          IconButton(
+            icon : const Icon(Icons.list) ,
+            onPressed : _pushSaved ,
+            tooltip: 'Saved Suggestions',
+          )
+        ]
+      ),
+      // #docregion itemBuilder
+      body: ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return const Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
+          // #docregion listTile
+          return ListTile(
+            title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved ? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved ? Colors.red : null,
+              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {
+              setState(() {
+                if (alreadySaved) {
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
+          );
+          // #enddocregion listTile
+        },
+      ),
+      // #enddocregion itemBuilder
+    );
+  }
+// #enddocregion RWS-build
+// #docregion RWS-var
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context){
+
+          final tiles = _saved.map(
+              (pair){
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  )
+                );
+              }
+          );
+
+          final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(
+              context : context,
+              tiles: tiles
+          ).toList()
+            : <Widget>[] ;
+
+          return Scaffold(
+            appBar : AppBar(
+              title : const Text('Saved Suggestions')
+            ),
+            body : ListView(children : divided)
+          );
+
+        }
+      )
+    ) ;
+  }
+
+}
+// #enddocregion RWS-var
 
 class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
 
   @override
-  _RandomWordsState createState() => _RandomWordsState();
+  State<RandomWords> createState() => _RandomWordsState();
 }
-
-class _RandomWordsState extends State<RandomWords> {
-  
-  final _suggestions = <WordPair>[] ;
-  final _saved = <WordPair>{} ; // NEW
-  final _biggerFont = TextStyle(fontSize: 18) ;
-
-  Widget _buildRow(WordPair pair){
-    final alreadySaved = _saved.contains(pair) ; // NEW
-    return ListTile(
-      title: Text(
-        pair.asPascalCase ,
-        style: _biggerFont
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color : alreadySaved ? Colors.red : null ,
-        semanticLabel: alreadySaved ? 'Remove from save' : 'Save',
-      )
-    ) ;
-  }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
